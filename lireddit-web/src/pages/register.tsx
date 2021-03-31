10cfa28ee6300b4f04d71e9def57d414919d0c95
ltobar <luisadrianta1@gmail.com>
2021-03-31 17:31:00 -0500

    MOD: handdle errors at register and route to home when loged in

--- lireddit-web/src/pages/register.tsx
+++ lireddit-web/src/pages/register.tsx
@@ -5,18 +5,30 @@ import { Wrapper } from "../components/Wrapper";
 import { InputField } from "../components/InputField";
 import { Box, Button } from "@chakra-ui/react";
 import { useRegisterMutation } from "../generated/graphql";
+import { toErrorMap } from "../utils/toErrorMap";
+import { useRouter } from "next/router";
 
 interface registerProps {}
 
 // In Next js the name of the files inside pages will be routes
 const Register: React.FC<registerProps> = ({}) => {
+  const router = useRouter();
   const [, register] = useRegisterMutation(); // first parameter is info about mutation, second is the funtion name
+
   return (
     <Wrapper variant="small">
       <Formik
         initialValues={{ username: "", password: "" }}
-        onSubmit={(values) => {
-          return register(values);
+        onSubmit={async (values, { setErrors }) => {
+          const response = await register(values);
+          // response.data.register.errors -> if there is no data will throw an error (break the app)
+          // response.data?.register.errors -> if ther is no data will throw undefine
+          // optional chaining from typescript
+          if (response.data?.register.errors) {
+            setErrors(toErrorMap(response.data.register.errors));
+          } else if (response.data?.register.user) {
+            router.push("/");
+          }
         }}
       >
         {({ isSubmitting }) => (
